8b09a7e133088fe51cc4e737bcba24195ed52b01
minnna11 <aungminedu@gmail.com>
2021-08-09 15:39:52 -0400

    home/cart page

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBO9iJnCQgAbn-Jn-lgh8YHtBRXU420UjQ",
+    authDomain: "asia-cafe-b97aa.firebaseapp.com",
+    projectId: "asia-cafe-b97aa",
+    storageBucket: "asia-cafe-b97aa.appspot.com",
+    messagingSenderId: "579229166694",
+    appId: "1:579229166694:web:42f097a1f0c88fc07911a7",
+    measurementId: "G-X53LGMJBTM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
